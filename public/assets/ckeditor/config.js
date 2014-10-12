cbf69aa499d2bc32a3a7a489502bf16bc3ea80f8
Rémi NUYTS <remiuz@MacBook-Pro-de-Remi.local>
2014-10-12 18:57:30 +0200

    probleme assets

--- public/assets/ckeditor/config.js
+++
@@ -1,8 +0,0 @@
-CKEDITOR.editorConfig = function( config ) {
-    config.toolbar = [
-   ['Styles','Format','Font','FontSize'],
-   ['Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-','Print'],
-   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-   ['Table','-','Link']];
-
-};
