ab84354bbc247a5a332a2edc2c173301ed6d3c05
Rémi NUYTS <remiuz@MacBook-Pro-de-Remi.local>
2014-10-12 21:14:11 +0200

    probleme assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w[tinymce/tiny_mce.js tinymce/langs/en.js tinymce/themes/advanced/editor_template.js tinymce/themes/modern/theme.js]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
